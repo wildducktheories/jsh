5002d841db4290c1f3efba771114575480e893f1
Jon Seymour <jon.seymour@gmail.com>
2014-02-13 19:58:09 +1100

    Allow j.sh to be invoked directly in which case package command is passed as first parameter.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- bin/j.sh
+++ bin/j.sh
@@ -9,8 +9,20 @@ die()
     echo "$*" 1>&2
     exit 1;
 }
-export JSH_PACKAGE=$(basename "$BASH_SOURCE")
-export JSH_RUNTIME=$(cd "$(dirname "$BASH_SOURCE")"; cd ..; pwd)
+if test -L "${BASH_SOURCE}"
+then
+    JSH_RUNTIME=$(ls -ld "${BASH_SOURCE}"); 
+    JSH_RUNTIME=${JSH_RUNTIME#*-> }; 
+    JSH_RUNTIME=${JSH_RUNTIME%/mnt/jsh/resolved-packages/jsh/bin/j.sh}; 
+    export JSH_PACKAGE=$(basename "$BASH_SOURCE")
+else
+    JSH_RUNTIME=$(cd "$(dirname "${BASH_SOURCE}")"; pwd); 
+    JSH_RUNTIME=${JSH_RUNTIME%/mnt/jsh/resolved-packages/jsh/bin}; 
+    export JSH_PACKAGE=$1
+    test -n "$JSH_PACKAGE" || die "usage: j.sh {package} {module...} {arg...}"
+    shift 1
+fi
+export JSH_RUNTIME
 export JSH_RESOLVED_PACKAGES=${JSH_RUNTIME}/mnt/jsh/resolved-packages
 export JSH_JSH_PACKAGE_DIR=${JSH_RESOLVED_PACKAGES}/jsh
 export JSH_JSH_MODULE_FILE=${JSH_JSH_PACKAGE_DIR}/jsh.j
@@ -18,6 +30,7 @@ export JSH_MODULE_STACK=${JSH_PACKAGE}
 test -f "${JSH_JSH_MODULE_FILE}" &&
 . "$JSH_JSH_MODULE_FILE" || {
    cat >&2 <<EOF
+BASH_SOURCE=${BASH_SOURCE}
 JSH_PACKAGE=${JSH_PACKAGE}
 JSH_RUNTIME=${JSH_RUNTIME}
 JSH_RESOLVED_PACKAGES=${JSH_RESOLVED_PACKAGES}
